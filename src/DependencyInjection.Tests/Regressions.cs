34d7295b202447659e055371613bf81ac5bf4aa1
Daniel Cazzulino <daniel@cazzulino.com>
2025-04-23 17:54:20 -0300

    Don't emit registrations with generics that don't satisfy constraints
    We were emitting instantiations of generic interfaces (implemented by the service) for covariant type parameters that didn't satisfy the generic type parameter constraints.

---
+++ src/DependencyInjection.Tests/Regressions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Spectre.Console.Cli;
+
+namespace Tests.Regressions;
+
+public class Regressions
+{
+    [Fact]
+    public void CovariantRegistrationSatisfiesIntefaceConstraints()
+    {
+        var collection = new ServiceCollection();
+        collection.AddServices(typeof(ICommand));
+
+        var provider = collection.BuildServiceProvider();
+
+        var command = provider.GetRequiredService<MyCommand>();
+
+        Assert.Equal(0, command.Execute(new CommandContext([], Mock.Of<IRemainingArguments>(), "my", null),
+            new MySetting { Base = "", Name = "" }));
+    }
+}
+
+public interface ISetting
+{
+    string Name { get; set; }
+}
+
+public class BaseSetting : CommandSettings
+{
+    [CommandArgument(0, "<BASE>")]
+    public required string Base { get; init; }
+}
+
+public class MySetting : BaseSetting, ISetting
+{
+    [CommandOption("--name")]
+    public required string Name { get; set; }
+}
+
+public class MyCommand : BaseCommand<MySetting> { }
+
+public abstract class BaseCommand<TSettings> : Command<TSettings> where TSettings : BaseSetting, ISetting
+{
+    public override int Execute(CommandContext context, TSettings settings)
+    {
+        Console.WriteLine($"Base: {settings.Base}, Name: {settings.Name}");
+        return 0;
+    }
+}
