15f540cc64d4e4d43b1c2fa5ea6d8024303b0b4e
Daniel Cazzulino <daniel@cazzulino.com>
2025-12-21 18:02:03 -0300

    Provide a codefix for legacy service usage and add tests
    The codefix should be helpful in migrating larger codebases that used the previous approach (since we allow batch fixing in an entire solution).
    
    The added project also means we need a separate packaging project (can't reference the codefix project from the analyzer one because that can cause the analyzer to not load in the reduced scope of the compiler). The codefix project is only intended by consumption by an editor that can apply fixes.
    
    We now have a standalone packaging project that contains the eula and readme plus references to the other two.

---
+++ src/CodeAnalysis.Tests/LegacyServiceAttributeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Devlooped.Extensions.DependencyInjection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using Xunit.Abstractions;
+using AnalyzerTest = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerTest<Devlooped.Extensions.DependencyInjection.LegacyServiceAttributeAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Devlooped.Extensions.DependencyInjection.LegacyServiceAttributeAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace Tests.CodeAnalysis;
+
+public class LegacyServiceAttributeTests(ITestOutputHelper Output)
+{
+    [Fact]
+    public async Task ErrorIfTKeyMatchesStringConstant()
+    {
+        var test = new AnalyzerTest
+        {
+            TestBehaviors = TestBehaviors.SkipGeneratedSourcesCheck,
+            TestCode =
+                """
+                using System;
+                using Microsoft.Extensions.DependencyInjection;
+    
+                [{|#0:Service<string>("my")|}]
+                public class MyService { }
+                """
+        }.WithTestState();
+
+        var expected = Verifier.Diagnostic(LegacyServiceAttributeAnalyzer.ServiceTypeNotKeyType).WithLocation(0);
+        test.ExpectedDiagnostics.Add(expected);
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ExecuteSyncWithAsyncCommand()
+    {
+        var test = new CSharpCodeFixTest<LegacyServiceAttributeAnalyzer, LegacyServiceAttributeFixer, DefaultVerifier>
+        {
+            TestBehaviors = TestBehaviors.SkipGeneratedSourcesCheck,
+            TestCode =
+                """
+                using System;
+                using Microsoft.Extensions.DependencyInjection;
+    
+                [{|#0:Service<string>("my")|}]
+                public class MyService { }
+                """,
+            FixedCode =
+                """
+                using System;
+                using Microsoft.Extensions.DependencyInjection;
+    
+                [Service("my")]
+                public class MyService { }
+                """,
+        }.WithTestState();
+
+        test.ExpectedDiagnostics.Add(new DiagnosticResult(LegacyServiceAttributeAnalyzer.ServiceTypeNotKeyType).WithLocation(0));
+        test.FixedState.Sources.AddStaticFiles();
+
+        await test.RunAsync();
+    }
+}
--- src/CodeAnalysis.Tests/TestExtensions.cs
+++ src/CodeAnalysis.Tests/TestExtensions.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Resources;
 
 namespace Tests.CodeAnalysis;
 
@@ -30,5 +33,27 @@ public static class TestExtensions
         });
 
         return test;
+    }
+
+    public static TAnalyzerTest WithTestState<TAnalyzerTest>(this TAnalyzerTest test)
+        where TAnalyzerTest : AnalyzerTest<DefaultVerifier>
+    {
+        test.TestState.Sources.AddStaticFiles();
+        test.TestState.ReferenceAssemblies = new ReferenceAssemblies(
+            "net8.0",
+            new PackageIdentity(
+                "Microsoft.NETCore.App.Ref", "8.0.0"),
+                Path.Combine("ref", "net8.0"))
+            .AddPackages(ImmutableArray.Create(
+                new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0")));
+
+        return test;
+    }
+
+    public static void AddStaticFiles(this SourceFileList sources)
+    {
+        sources.Add(ThisAssembly.Resources.AddServicesNoReflectionExtension.Text);
+        sources.Add(ThisAssembly.Resources.ServiceAttribute.Text);
+        sources.Add(ThisAssembly.Resources.ServiceAttribute_1.Text);
     }
 }
