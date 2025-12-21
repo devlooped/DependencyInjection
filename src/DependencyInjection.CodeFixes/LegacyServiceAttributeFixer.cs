88a0a37b915556b7ec66ecfe83f712e8c9534d23
Daniel Cazzulino <daniel@cazzulino.com>
2025-12-21 18:02:03 -0300

    Provide a codefix for legacy service usage and add tests
    The codefix should be helpful in migrating larger codebases that used the previous approach (since we allow batch fixing in an entire solution).
    
    The added project also means we need a separate packaging project (can't reference the codefix project from the analyzer one because that can cause the analyzer to not load in the reduced scope of the compiler). The codefix project is only intended by consumption by an editor that can apply fixes.
    
    We now have a standalone packaging project that contains the eula and readme plus references to the other two.

---
+++ src/DependencyInjection.CodeFixes/LegacyServiceAttributeFixer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Devlooped.Extensions.DependencyInjection;
+
+[Shared]
+[ExportCodeFixProvider(LanguageNames.CSharp)]
+public class LegacyServiceAttributeFixer : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(
+        LegacyServiceAttributeAnalyzer.ServiceTypeNotKeyType.Id);
+
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        if (root == null)
+            return;
+
+        var attribute = root.FindNode(context.Span).FirstAncestorOrSelf<AttributeSyntax>();
+        if (attribute == null)
+            return;
+
+        context.RegisterCodeFix(new RemoveGenericParameterCodeAction(context.Document, attribute), context.Diagnostics);
+    }
+
+    class RemoveGenericParameterCodeAction(Document document, AttributeSyntax syntax) : CodeAction
+    {
+        public override string Title => "Remove generic parameter";
+        public override string? EquivalenceKey => Title;
+
+        protected override async Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            if (root == null)
+                return document;
+
+            var isGeneric = syntax.Name is GenericNameSyntax;
+            if (!isGeneric)
+                return document;
+
+            var newName = IdentifierName("Service");
+
+            return document.WithSyntaxRoot(root.ReplaceNode(syntax, syntax.WithName(newName)));
+        }
+    }
+}
